37da02d50d14a0fefe01fe6879e615a0a6468c15
KimSeonMin00 <103050847+KimSeonMin00@users.noreply.github.com>
2026-01-11 21:51:10 +0900

    설치 아이템 추가

---
+++ Assets/Project/Scripts/GamePlay/Place/Smeltor/Smeltor.cs
@@ -0,0 +1,15 @@
+using Mono.Cecil;
+using UnityEngine;
+
+public class Smeltor : Building
+{
+    public override void OnInteract()
+    {
+        return;
+    }
+
+    public override void RecalculateBonus()
+    {
+        
+    }
+}
